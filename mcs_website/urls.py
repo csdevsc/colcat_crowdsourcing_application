54b9c53cb7ae91af816e2bd1e5d79d120552a7cc
Stephanie Chang <csdev.sc@gmail.com>
2015-03-27 03:08:23 -0700

    Implement base template and user authentication
    Add initial test data, create index and about page, configure user authentication for django-registration-redux

--- mcs_website/urls.py
+++ mcs_website/urls.py
@@ -1,10 +1,13 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
+from crowdsource.views import MyRegistrationView
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mcs_website.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', RedirectView.as_view(url='/crowdsource/')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^crowdsource/', include('crowdsource.urls')),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 )
