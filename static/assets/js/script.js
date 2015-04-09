8c8dbb1b270cc2783c887af5a17b31f1dc8ef932
Stephanie Chang <csdev.sc@gmail.com>
2015-04-08 17:26:04 -0700

    Update tasks from initial feedback

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,7 +1,7 @@
 function toggle(id) {
    var e = document.getElementById(id);
-   if(e.style.display == 'block')
-      e.style.display = 'none';
-   else
+   if(e.style.display == 'none')
       e.style.display = 'block';
+   else
+      e.style.display = 'none';
 }
