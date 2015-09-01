3d4597dfa4449bd156da2ca0c0fd74a51523f80e
Stephanie Chang <csdev.sc@gmail.com>
2015-09-01 15:57:23 -0700

    Update prescreen task
    Use client-side JavaScript validation

--- tasks/models/tasks.py
+++ tasks/models/tasks.py
@@ -6,36 +6,17 @@ Each new type of task corresponds to a task model
 from django.db import models
 from data import Data_FullGrid_Confidence, Data_FullGrid
 
-class Task_Prescreen_01(models.Model):
+# Practice Tasks
+class Task_Practice_Foci_01(models.Model):
     class Meta:
-        db_table = 'tbl_response_prescreen'
+        db_table = 'tbl_response_practice_foci'
     def __unicode__(self):
-        return 'Prescreen task'
-    task_response_id = models.AutoField(primary_key=True, unique=True)
+        return 'Foci Practice Task'
+    task_response_id = models.AutoField(primary_key = True, unique=True)
     worker_id = models.CharField(max_length=128)
-    task_response_key = models.CharField(max_length=32, unique=True)
 
-    answer_01 = models.CharField(max_length=16)
-    answer_02 = models.CharField(max_length=16)
-    answer_03 = models.CharField(max_length=16)
-    answer_04 = models.CharField(max_length=16)
-    answer_05 = models.CharField(max_length=16)
-    answer_06 = models.CharField(max_length=16)
-    answer_07 = models.CharField(max_length=16)
-    answer_08 = models.CharField(max_length=16)
-    answer_09 = models.CharField(max_length=16)
-    answer_10 = models.CharField(max_length=16)
-    answer_11 = models.CharField(max_length=16)
-    answer_12 = models.CharField(max_length=16)
-    answer_13 = models.CharField(max_length=16)
-    answer_14 = models.CharField(max_length=16)
-    answer_15 = models.CharField(max_length=16)
-    answer_16 = models.CharField(max_length=16)
-    answer_17 = models.CharField(max_length=16)
-    answer_18 = models.CharField(max_length=16)
-    answer_19 = models.CharField(max_length=16)
-    answer_20 = models.CharField(max_length=16)
 
+# Tasks
 class Task_Naming_001(Data_FullGrid_Confidence):
     class Meta:
         db_table = 'tbl_response_naming_001'
