1c207990bf707d2bf299d0c98ee8729cf4ba831b
Stephanie Chang <csdev.sc@gmail.com>
2015-09-29 17:32:46 -0700

    Update template css

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
-# Register your models here.
+# from import_export import resources
+# from import_export.admin import ImportExportModelAdmin
+# from tasks.models import Task_Foci_001
+#
+# class Task_Foci_001_Resource(resources.ModelResource):
+#     class Meta:
+#         model = Task_Foci_001
+#
+# # ADMINS
+# class Task_Foci_001_Admin(admin.ModelAdmin):
+#     resource_class = Task_Foci_001_Resource
+#     pass
+#
+# admin.site.register(Task_Foci_001, Task_Foci_001_Admin)
